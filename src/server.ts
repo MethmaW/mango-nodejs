6b5907afb9cb98e3b91c909d3fa2344791cc94f6
Methma W <methma-w@Methmas-MacBook-Pro.local>
2021-07-27 01:53:52 +0530

    Remove seed

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
 import App from '@/app';
+import validateEnv from '@utils/validateEnv';
+
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import RoomsRoute from './routes/rooms.route';
-import validateEnv from '@utils/validateEnv';
-
-import roomModel from '@models/rooms.model'
-import paymentMethodModel from "@models/payment-methods.model"
 
 validateEnv();
 
@@ -19,35 +17,6 @@ const app = new App([
     new UsersRoute(),
     new RoomsRoute()
 ]);
-
-const createRoom = async () => {
-    const createUserData = await roomModel.create({
-        propertyId: "60fef52e060b9ea27ac4e13e",
-        occupancy: "Single",
-        amenities: ["Sea view", "Balcony", "Floor area(500m sqft)", "Wifi"],
-        rates: [{
-            bookingType: "Bed & Breakfast",
-            rate: 15.00
-        }, {
-            bookingType: "Half-Board",
-                rate: 24.00
-            }, {
-            bookingType: "Full-Board",
-                rate: 30.00
-            }],
-        defaultCheckin: 13,
-        defaultCheckout: 11,
-    });
-
-    console.log("vhhg", createUserData);
-    
-}
-
-const createPaymentMethod = async () => {
-    const createUserData = await paymentMethodModel.create([{ name: "credit card online" }, { name: "credit card at the location" }, { name: "cash payments at the location" }])
-}
-
-
 
 
 app.listen();
